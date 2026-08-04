7230a639fb3ffdc63bdff4d52936c9f062bff46f
mukesh3karthi <mukeshkarthikn333@gmail.com>
2026-08-04 12:12:03 +0530

    Fix lowercase ownvehicle import

--- Back-End/controllers/ownvehicleController.js
+++ Back-End/controllers/ownvehicleController.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const OwnVehicle = require("../models/ownVehicle");
+const OwnVehicle = require("../models/ownvehicle");
 
 /* ==========================================
    Upload path
